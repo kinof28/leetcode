e7da938cc0eeb8eb09583835a4fd02c4694becdd
Abdelwahab <ab28fb@gmail.com>
2022-11-04 01:44:54 +0100

    naive solution

--- 724. Find Pivot Index/Solution.java
+++ 724. Find Pivot Index/Solution.java
@@ -1,5 +1,17 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        return 0;
+        int sum = 0, reversedSum = 0, length = nums.length;
+        int[] sums = new int[length], reversedSums = new int[length];
+        for (int i = 0; i < length; i++) {
+            sums[i] = sum;
+            reversedSums[i] = reversedSum;
+            sum += nums[i];
+            reversedSum += nums[length - i - 1];
+        }
+        for (int i = 0; i < length; i++) {
+            if (sums[i] == reversedSums[length - i - 1])
+                return i;
+        }
+        return -1;
     }
 }
