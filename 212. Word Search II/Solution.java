f7e09883ed299644ae7c70b0b484bd7bb9b41139
Abdelwahab <ab28fb@gmail.com>
2022-11-05 02:34:05 +0100

    init

--- 212. Word Search II/Solution.java
+++ 212. Word Search II/Solution.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public List<String> findWords(char[][] board, String[] words) {
-        
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[][] board = { { 'o', 'a', 'a', 'n' }, { 'e', 't', 'a', 'e' }, { 'i', 'h', 'k', 'r' },
+                { 'i', 'f', 'l', 'v' } };
+        String[] words = { "oath", "pea", "eat", "rain" };
+        System.out.println(
+                solution.findWords(board, words).toString());
     }
-    
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        for (String word : words) {
+            if (wordExists(board, word))
+                result.add(word);
+        }
+        return result;
+    }
+
+    public boolean wordExists(char[][] board, String word) {
+        return true;
+    }
 }
