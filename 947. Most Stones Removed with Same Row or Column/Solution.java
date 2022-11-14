cb61723ad1742f3a9630938df681bc1bfdaef789
Abdelwahab <ab28fb@gmail.com>
2022-11-14 12:07:41 +0100

    init

---
+++ 947. Most Stones Removed with Same Row or Column/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[][] stones = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 2 }, { 2, 1 }, { 2, 2 } };
+        Solution solution = new Solution();
+        System.out.println("result: " + solution.removeStones(stones));
+    }
+
+    public int removeStones(int[][] stones) {
+       Queue<int[]> queue=new LinkedList<>();
+
+        return 0;
+    }
+}
