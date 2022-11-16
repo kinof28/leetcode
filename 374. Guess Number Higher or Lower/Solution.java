26ffe31795cc8acaf9c95ea0ac03925239fa7e75
Abdelwahab <ab28fb@gmail.com>
2022-11-16 19:09:31 +0100

    374. Guess Number Higher or Lower

--- 374. Guess Number Higher or Lower/Solution.java
+++ 374. Guess Number Higher or Lower/Solution.java
@@ -1,39 +1,28 @@
 public class Solution {
 
-    public static void main(String[] args) {
-
-    }
-
     public int guessNumber(int n) {
-        int guessedNumber = (n / 2) + n % 2;
-        int result = guess(guessedNumber);
+        long min = 0, max = n;
+        long guessedNumber = (min + max) / 2 + (min + max) % 2;
+        int result = guess((int) guessedNumber);
         while (result != 0) {
             if (result == 1) {
-                if (((n - guessedNumber) / 2) == 0)
-                    guessedNumber++;
-                else
-                    guessedNumber = guessedNumber + ((n - guessedNumber) / 2);
+                min = guessedNumber;
             } else {
-                if (((n - guessedNumber) / 2) == 0)
-                    guessedNumber--;
-                else
-                    guessedNumber = guessedNumber - ((n - guessedNumber) / 2);
+                max = guessedNumber;
             }
-            result = guess(guessedNumber);
+            guessedNumber = (min + max) / 2 + (min + max) % 2;
+            result = guess((int) guessedNumber);
         }
-        return guessedNumber;
+        return (int) guessedNumber;
     }
 
-    /**
-     * Forward declaration of guess API.
-     * 
-     * @param num your guess
-     * @return -1 if num is higher than the picked number
-     *         1 if num is lower than the picked number
-     *         otherwise return 0
-     *         int guess(int num);
-     */
     public int guess(int num) {
-        return 0;
+        int param = 1702766719;
+        if (num < param)
+            return 1;
+        else if (num > param)
+            return -1;
+        else
+            return 0;
     }
 }
