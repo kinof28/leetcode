442bd98d208eaa4ab4bcf1b233c9ab0963b3af3f
Abdelwahab <ab28fb@gmail.com>
2022-11-17 19:13:53 +0100

    init

---
+++ 223. Rectangle Area/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.computeArea(-3, 0, 3, 4, 0, -1, 9, 2));
+    }
+
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+
+        return 0;
+    }
+}
