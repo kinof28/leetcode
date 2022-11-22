0df40fe38e42822525b93bd22938143a7288627d
Abdelwahab <ab28fb@gmail.com>
2022-11-22 23:30:24 +0100

    WIP

--- 4. Median of Two Sorted Arrays/Solution.java
+++ 4. Median of Two Sorted Arrays/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int totalLength = nums1.length + nums2.length;
-
+        int[] Longest = nums1.length > nums2.length ? nums1 : nums2;
         return 0;
     }
 }
