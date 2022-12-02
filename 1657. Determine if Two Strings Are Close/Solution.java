9d56b90b57c06da4e6d9a7b9c7d3cc0d90a04141
Abdelwahab <ab28fb@gmail.com>
2022-12-02 12:07:34 +0100

    1657. Determine if Two Strings Are Close

--- 1657. Determine if Two Strings Are Close/Solution.java
+++ 1657. Determine if Two Strings Are Close/Solution.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        return false;
+        if (word1.length() != word2.length())
+            return false;
+        int[] firstLettersFrequency = new int[26];
+        int[] secondLettersFrequency = new int[26];
+
+        for (char c : word1.toCharArray()) {
+            firstLettersFrequency[c - 'a']++;
+        }
+        for (char c : word2.toCharArray()) {
+            secondLettersFrequency[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if ((firstLettersFrequency[i] != 0 && secondLettersFrequency[i] == 0)
+                    || (firstLettersFrequency[i] == 0 && secondLettersFrequency[i] != 0))
+                return false;
+        }
+        Arrays.sort(firstLettersFrequency);
+        Arrays.sort(secondLettersFrequency);
+        return Arrays.compare(firstLettersFrequency, secondLettersFrequency) == 0;
+
     }
 }
