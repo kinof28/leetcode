17c51900e9bd572b37e74c5e4b0a1504721bfce0
Abdelwahab <ab28fb@gmail.com>
2022-12-03 20:42:19 +0100

    451. Sort Characters By Frequency

--- 451. Sort Characters By Frequency/Solution.java
+++ 451. Sort Characters By Frequency/Solution.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public String frequencySort(String s) {
-        return s;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c))
+                map.replace(c, map.get(c) + 1);
+            else
+                map.put(c, 1);
+        }
+        Object[] values = map.values().toArray();
+        Arrays.sort(values);
+        String result = "";
+        char ch = '.';
+        for (int i = 0; i < values.length; i++) {
+            for (char c : map.keySet()) {
+                if (map.get(c) == values[i]) {
+                    ch = c;
+                    break;
+                }
+            }
+            map.remove(ch);
+            for (int j = 0; j < (int) values[i]; j++) {
+                result = ch + result;
+            }
+        }
+        return result;
     }
 }
