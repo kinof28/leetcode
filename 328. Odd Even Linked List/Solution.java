d976282485e95ee794407dd1d148f1839da74ff6
Abdelwahab <ab28fb@gmail.com>
2022-12-06 16:13:30 +0100

    328. Odd Even Linked List

--- 328. Odd Even Linked List/Solution.java
+++ 328. Odd Even Linked List/Solution.java
@@ -1,5 +1,29 @@
 class Solution {
+
     public ListNode oddEvenList(ListNode head) {
+        ListNode index = head, lastNode = null, temp = null;
+        int size = 0;
+        int i = 1;
+        while (index != null) {
+            size++;
+            if (index.next == null)
+                lastNode = index;
+            index = index.next;
+
+        }
+        index = head;
+        while (index != null && index.next != null && i < size) {
+            if (i % 2 == 1 && size != 2) {
+                temp = index.next;
+                index.next = temp.next;
+                temp.next = null;
+                lastNode.next = temp;
+                lastNode = temp;
+                i++;
+            }
+            index = index.next;
+            i++;
+        }
         return head;
     }
 }
