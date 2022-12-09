ef531cc751448cff36a44a6a455dabeaa6256615
Abdelwahab <ab28fb@gmail.com>
2022-12-09 12:41:05 +0100

    1026. Maximum Difference Between Node and Ancestor

--- 1026. Maximum Difference Between Node and Ancestor/Solution.java
+++ 1026. Maximum Difference Between Node and Ancestor/Solution.java
@@ -1,5 +1,17 @@
 class Solution {
     public int maxAncestorDiff(TreeNode root) {
-        return 0;
+        int v = 0;
+        if (root == null || (root.left == null && root.right == null))
+            return v;
+        v = maxDiff(root.val, root);
+        return Math.max(v, Math.max(maxAncestorDiff(root.left), maxAncestorDiff(root.right)));
+    }
+
+    public int maxDiff(int rootVal, TreeNode current) {
+        if (current == null)
+            return Integer.MIN_VALUE;
+        else
+            return Math.max(Math.abs(rootVal - current.val),
+                    Math.max(maxDiff(rootVal, current.left), maxDiff(rootVal, current.right)));
     }
 }
