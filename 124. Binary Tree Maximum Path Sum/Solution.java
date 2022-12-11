472ddb71af37771cd517c2e783840df1abbcb156
Abdelwahab <ab28fb@gmail.com>
2022-12-11 06:18:45 +0100

    WIP

--- 124. Binary Tree Maximum Path Sum/Solution.java
+++ 124. Binary Tree Maximum Path Sum/Solution.java
@@ -1,11 +1,25 @@
+import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
 
-    Map<TreeNode, Long> treeSizes;
+    Map<TreeNode, Integer> treeSizes = new HashMap<>();
 
     public int maxPathSum(TreeNode root) {
-        return 0;
+        if (root == null)
+            return Integer.MIN_VALUE;
+        return (int) Math.max(treeSum(root), Math.max(maxPathSum(root.left), maxPathSum(root.right)));
     }
 
+    public int treeSum(TreeNode root) {
+        if (root == null)
+            return 0;
+        else if (treeSizes.containsKey(root))
+            return treeSizes.get(root);
+        else {
+            int result = root.val + treeSum(root.left) + treeSum(root.right);
+            treeSizes.put(root, result);
+            return result;
+        }
+    }
 }
