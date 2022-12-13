ffd078deed663ea51ff246e2527792bba24add15
Abdelwahab <ab28fb@gmail.com>
2022-12-13 19:15:16 +0100

    931. Minimum Falling Path Sum

--- 931. Minimum Falling Path Sum/Solution.java
+++ 931. Minimum Falling Path Sum/Solution.java
@@ -1,5 +1,22 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        return 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (j == 0)
+                    matrix[i][j] = matrix[i][j] + Math.min(matrix[i - 1][j], matrix[i - 1][j + 1]);
+                else if (j == matrix.length - 1)
+                    matrix[i][j] = matrix[i][j] + Math.min(matrix[i - 1][j], matrix[i - 1][j - 1]);
+                else
+                    matrix[i][j] = matrix[i][j]
+                            + Math.min(matrix[i - 1][j], Math.min(matrix[i - 1][j - 1], matrix[i - 1][j + 1]));
+            }
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[matrix.length - 1][i] < min)
+                min = matrix[matrix.length - 1][i];
+        }
+        return min;
     }
+
 }
