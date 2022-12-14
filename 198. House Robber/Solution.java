d4b3fcef292fd6864f162261679241a81abf6bbb
Abdelwahab <ab28fb@gmail.com>
2022-12-14 22:14:44 +0100

    198. House Robber

--- 198. House Robber/Solution.java
+++ 198. House Robber/Solution.java
@@ -1,5 +1,13 @@
 class Solution {
+
     public int rob(int[] nums) {
-        
+        int[] dp = new int[nums.length];
+        dp[0] = 0;
+        dp[1] = nums[1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+        return dp[dp.length - 1];
     }
+
 }
