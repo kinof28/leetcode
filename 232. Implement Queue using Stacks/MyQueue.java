b1ef5a3962dbabe44756411c08ca3275c83bb4b4
Abdelwahab <ab28fb@gmail.com>
2022-12-16 12:35:14 +0100

    init

---
+++ 232. Implement Queue using Stacks/MyQueue.java
@@ -0,0 +1,31 @@
+class MyQueue {
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+        return 0;
+    }
+
+    public int peek() {
+        return 0;
+    }
+
+    public boolean empty() {
+        return true;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
