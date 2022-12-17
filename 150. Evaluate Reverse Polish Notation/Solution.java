335dc4e79e302ff5287685b279036cc08d1b289f
Abdelwahab <ab28fb@gmail.com>
2022-12-17 15:22:13 +0100

    150. Evaluate Reverse Polish Notation

--- 150. Evaluate Reverse Polish Notation/Solution.java
+++ 150. Evaluate Reverse Polish Notation/Solution.java
@@ -1,58 +1,36 @@
-
-// import java.lang.reflect.Array;
-import java.util.Arrays;
-// import java.util.stream.Stream;
+import java.util.Stack;
 
 class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println("result array: " +
                 solution.evalRPN(new String[] { "4", "13", "5", "/", "+" }));
-        // System.out.println("result String: " +
-        // solution.evalRPNString("21+3*"));
     }
 
     public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
         if (tokens.length == 0)
             return 0;
         else if (tokens.length == 1)
             return Integer.parseInt(tokens[0]);
         else
             for (int i = 0; i < tokens.length; i++) {
-
-                try {
-                    Integer.parseInt(tokens[i]);
-                } catch (NumberFormatException exception) {
-
+                if (tokens[i].equals("+") || tokens[i].equals("*") || tokens[i].equals("-") || tokens[i].equals("/")) {
+                    int v2 = stack.pop(), v1 = stack.pop();
                     if (tokens[i].equals("+")) {
-
-                        tokens[i - 2] = Integer.parseInt(tokens[i - 2]) + Integer.parseInt(tokens[i - 1]) + "";
-
+                        stack.add(v1 + v2);
                     } else if (tokens[i].equals("-")) {
-
-                        tokens[i - 2] = Integer.parseInt(tokens[i - 2]) - Integer.parseInt(tokens[i - 1]) + "";
-
+                        stack.add(v1 - v2);
                     } else if (tokens[i].equals("*")) {
-
-                        tokens[i - 2] = Integer.parseInt(tokens[i - 2]) * Integer.parseInt(tokens[i - 1]) + "";
-
+                        stack.add(v1 * v2);
                     } else {
-                        int val = Integer.parseInt(tokens[i - 1]) != 0 ? Integer.parseInt(tokens[i - 1]) : 1;
-                        tokens[i - 2] = Integer.parseInt(tokens[i - 2]) / val + "";
+                        stack.add(v1 / v2);
                     }
-                    return evalRPN(this.concatenateArrays(Arrays.copyOfRange(tokens, 0, i - 1),
-                            Arrays.copyOfRange(tokens, i + 1, tokens.length)));
-
+                } else {
+                    stack.add(Integer.parseInt(tokens[i]));
                 }
-
             }
-        return 0;
-    }
-
-    public String[] concatenateArrays(String[] array1, String[] array2) {
-        String[] result = (Arrays.toString(array1).replace("[", "").replace("]", "").replace(" ", "") + ","
-                + Arrays.toString(array2).replace("[", "").replace("]", "").replace(" ", "")).split(",");
-        return result;
+        return stack.pop();
     }
 
 }
