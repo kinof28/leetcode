a9100b2fa0bcc9eb0ae09d18e82dd137afa5a94c
Abdelwahab <ab28fb@gmail.com>
2022-12-17 15:11:04 +0100

    WIP: still time limit exceded

--- 150. Evaluate Reverse Polish Notation/Solution.java
+++ 150. Evaluate Reverse Polish Notation/Solution.java
@@ -6,8 +6,10 @@ import java.util.Arrays;
 class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(
-                solution.evalRPN(new String[] { "2", "1", "+", "3", "*" }));
+        System.out.println("result array: " +
+                solution.evalRPN(new String[] { "4", "13", "5", "/", "+" }));
+        // System.out.println("result String: " +
+        // solution.evalRPNString("21+3*"));
     }
 
     public int evalRPN(String[] tokens) {
@@ -17,18 +19,20 @@ class Solution {
             return Integer.parseInt(tokens[0]);
         else
             for (int i = 0; i < tokens.length; i++) {
+
                 try {
                     Integer.parseInt(tokens[i]);
                 } catch (NumberFormatException exception) {
-                    if (tokens[i] == "+") {
+
+                    if (tokens[i].equals("+")) {
 
                         tokens[i - 2] = Integer.parseInt(tokens[i - 2]) + Integer.parseInt(tokens[i - 1]) + "";
 
-                    } else if (tokens[i] == "-") {
+                    } else if (tokens[i].equals("-")) {
 
                         tokens[i - 2] = Integer.parseInt(tokens[i - 2]) - Integer.parseInt(tokens[i - 1]) + "";
 
-                    } else if (tokens[i] == "*") {
+                    } else if (tokens[i].equals("*")) {
 
                         tokens[i - 2] = Integer.parseInt(tokens[i - 2]) * Integer.parseInt(tokens[i - 1]) + "";
 
@@ -38,27 +42,17 @@ class Solution {
                     }
                     return evalRPN(this.concatenateArrays(Arrays.copyOfRange(tokens, 0, i - 1),
                             Arrays.copyOfRange(tokens, i + 1, tokens.length)));
+
                 }
+
             }
         return 0;
     }
 
     public String[] concatenateArrays(String[] array1, String[] array2) {
-        String[] result = new String[array1.length + array2.length];
-        for (int i = 0; i < array1.length; i++) {
-            result[i] = array1[i];
-        }
-        for (int i = 0; i < array2.length; i++) {
-            result[array1.length + i] = array2[i];
-        }
-        System.out.println(Arrays.toString(result));
+        String[] result = (Arrays.toString(array1).replace("[", "").replace("]", "").replace(" ", "") + ","
+                + Arrays.toString(array2).replace("[", "").replace("]", "").replace(" ", "")).split(",");
         return result;
-        // return concatWithStream(array1, array2);
     }
 
-    // private static <T> T[] concatWithStream(T[] array1, T[] array2) {
-    // return Stream.concat(Arrays.stream(array1), Arrays.stream(array2))
-    // .toArray(size -> (T[])
-    // Array.newInstance(array1.getClass().getComponentType(), size));
-    // }
 }
