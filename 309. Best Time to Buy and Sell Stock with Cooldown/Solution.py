67cd7b94c56ca8674982cd400bd001d87db5fb7b
Abdelwahab <ab28fb@gmail.com>
2022-12-24 15:21:11 +0100

    python init

---
+++ 309. Best Time to Buy and Sell Stock with Cooldown/Solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        print("hello world!")
+
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+
+solution = Solution()
+solution.maxProfit([])
