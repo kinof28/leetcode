3588613ee115338c356c6c9e8450cfb503ec1001
Abdelwahab <ab28fb@gmail.com>
2022-12-25 04:15:42 +0100

    790. Domino and Tromino Tiling

--- 790. Domino and Tromino Tiling/Solution.py
+++ 790. Domino and Tromino Tiling/Solution.py
@@ -1,6 +1,24 @@
-class Solution(object):
-    def numTilings(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+from typing import List
+
+
+class Solution:
+    def numTilings(self, n: int) -> int:
+        return self.numTilingsRecursively(n, [0]*(n+1))
+
+    def numTilingsRecursively(self, n: int, dp: List[int]) -> int:
+        if (n < 0):
+            return 0
+        if (n == 0):
+            return 1
+        if (n < 3):
+            return n
+        if (n == 3):
+            return 5
+        if (not dp[n] == 0):
+            return int(dp[n] % (1e9 + 7))
+        else:
+            result = 2 * \
+                self.numTilingsRecursively(
+                    n-1, dp)+self.numTilingsRecursively(n-3, dp)
+            dp[n] = result
+            return int(result % (1e9 + 7))
