c6a19be3cc165cfcafce0bda263272bd84ff0d72
Abdelwahab <ab28fb@gmail.com>
2022-12-25 05:27:45 +0100

    309. Best Time to Buy and Sell Stock with Cooldown

--- 309. Best Time to Buy and Sell Stock with Cooldown/Solution.py
+++ 309. Best Time to Buy and Sell Stock with Cooldown/Solution.py
@@ -1,12 +1,22 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        print("hello world!")
-
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+from typing import List
 
 
-solution = Solution()
-solution.maxProfit([])
+class Solution:
+
+    def maxProfit(self, prices: List[int]) -> int:
+        return self.dfs(prices, 0, True, {})
+
+    def dfs(self, prices: List[int], index: int, buying: bool, profits) -> int:
+        if (index >= len(prices)):
+            return 0
+        if (index, buying) in profits:
+            return profits[(index, buying)]
+        coolDown = self.dfs(prices, index+1, buying, profits)
+        if (buying):
+            result = self.dfs(prices, index+1, not buying,
+                              profits)-prices[index]
+        else:
+            result = self.dfs(prices, index+2, not buying,
+                              profits)+prices[index]
+        profits[(index, buying)] = max(result, coolDown)
+        return profits[(index, buying)]
