e6ac3042eb23b96a5b2af022e68bde0a07023d1b
Abdelwahab <ab28fb@gmail.com>
2022-12-25 03:46:26 +0100

    2389. Longest Subsequence With Limited Sum

--- 2389. Longest Subsequence With Limited Sum/Solution.java
+++ 2389. Longest Subsequence With Limited Sum/Solution.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 class Solution {
+
     public int[] answerQueries(int[] nums, int[] queries) {
-        return null;
+        int[] result = new int[queries.length];
+        Arrays.sort(nums);
+        for (int i = 0; i < result.length; i++) {
+            result[i] = maxLength(nums, queries[i]);
+        }
+        return result;
+    }
+
+    public int maxLength(int[] nums, int query) {
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum <= query) {
+                result++;
+            } else {
+                break;
+            }
+        }
+        return result;
     }
 }
--- 2389. Longest Subsequence With Limited Sum/Solution.py
+++ 2389. Longest Subsequence With Limited Sum/Solution.py
@@ -1,3 +1,22 @@
+from typing import List
+
+
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        return None
+        result = []
+        nums.sort()
+        for query in queries:
+            result.append(self.answerQuery(nums, query))
+
+        return result
+
+    def answerQuery(self, nums: List[int], query: int) -> int:
+        sum = 0
+        result = 0
+        for num in nums:
+            sum += num
+            if (sum > query):
+                break
+            else:
+                result = result+1
+        return result
