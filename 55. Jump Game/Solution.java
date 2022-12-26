64dbd2714a56d9aedbd5ab05c3fd5c89bd9f0e7d
Abdelwahab <ab28fb@gmail.com>
2022-12-26 15:50:25 +0100

    55. Jump Game

--- 55. Jump Game/Solution.java
+++ 55. Jump Game/Solution.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
+
     public boolean canJump(int[] nums) {
-        return false;
+        List<Integer> zeros = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0)
+                zeros.add(i);
+        }
+        for (int i = 0; i < zeros.size(); i++) {
+            for (int j = zeros.get(i) - 1; j >= 0; j--) {
+                if (j + nums[j] > zeros.get(i)) {
+                    zeros.remove(i);
+                    i--;
+                    break;
+                }
+            }
+            if (zeros.isEmpty())
+                break;
+        }
+        return zeros.isEmpty();
     }
 }
