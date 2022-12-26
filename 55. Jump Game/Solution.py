f84258105807e82842eaa11baccee9ee31b40a49
Abdelwahab <ab28fb@gmail.com>
2022-12-26 21:59:52 +0100

    55. Jump Game

--- 55. Jump Game/Solution.py
+++ 55. Jump Game/Solution.py
@@ -3,4 +3,16 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        return False
+        zeros = []
+        for i in range(len(nums)-1):
+            if (nums[i] == 0):
+                zeros.append(i)
+        for i in range(len(zeros)):
+            for j in range(zeros[i]-1, -1, -1):
+                if (nums[j]+j > zeros[i]):
+                    zeros[i] = -1
+                    break
+        for zero in zeros:
+            if zero != -1:
+                return False
+        return True
