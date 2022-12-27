4e796767e881a2f0071e55ba25bd5a62fccc1f6a
Abdelwahab <ab28fb@gmail.com>
2022-12-27 15:08:24 +0100

    2279. Maximum Bags With Full Capacity of Rocks

--- 2279. Maximum Bags With Full Capacity of Rocks/Solution.java
+++ 2279. Maximum Bags With Full Capacity of Rocks/Solution.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        return 0;
+        int diff[] = new int[capacity.length];
+        int sum = 0;
+        for (int i = 0; i < diff.length; i++) {
+            diff[i] = capacity[i] - rocks[i];
+        }
+        Arrays.sort(diff);
+        for (int i = 0; i < diff.length; i++) {
+            additionalRocks -= diff[i];
+            if (additionalRocks >= 0) {
+                sum++;
+            } else
+                break;
+        }
+        return sum;
     }
 }
