67576c6ed09593ffb83396e7a2e1d7dd5e79b58c
Abdelwahab <ab28fb@gmail.com>
2022-12-27 15:13:33 +0100

    2279. Maximum Bags With Full Capacity of Rocks

--- 2279. Maximum Bags With Full Capacity of Rocks/Solution.py
+++ 2279. Maximum Bags With Full Capacity of Rocks/Solution.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
-        pass
+        differences = []
+        for i in range(len(capacity)):
+            differences.append(capacity[i]-rocks[i])
+        differences.sort()
+        sum = 0
+        for difference in differences:
+            additionalRocks = additionalRocks-difference
+            if (additionalRocks >= 0):
+                sum = sum+1
+            else:
+                break
+        return sum
