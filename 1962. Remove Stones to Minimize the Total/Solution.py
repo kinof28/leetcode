c43483affdff2c5fa7dd527b871bd59ff7a709ee
Abdelwahab <ab28fb@gmail.com>
2022-12-28 20:43:51 +0100

    1962. Remove Stones to Minimize the Total

--- 1962. Remove Stones to Minimize the Total/Solution.py
+++ 1962. Remove Stones to Minimize the Total/Solution.py
@@ -1,6 +1,20 @@
+import math
+from queue import PriorityQueue
 from typing import List
 
 
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
-        return 0
+        pq = PriorityQueue()
+        for pile in piles:
+            pq.put(-pile)
+        for i in range(k):
+            val = pq.get()
+            val = val+math.floor(-val/2)
+            pq.put(val)
+        sum = 0
+        while not pq.empty():
+            val = pq.get()
+            print(val)
+            sum = sum-val
+        return sum
