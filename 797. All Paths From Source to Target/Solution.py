6181fb5401cbc100241904d5afb395c10f00d7d3
Abdelwahab <ab28fb@gmail.com>
2022-12-31 00:42:05 +0100

    797. All Paths From Source to Target

--- 797. All Paths From Source to Target/Solution.py
+++ 797. All Paths From Source to Target/Solution.py
@@ -1,6 +1,25 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        pass
+        result = []
+        graphMap = defaultdict(list)
+        for i in range(len(graph)):
+            graphMap[i] = graph[i]
+
+        def test_road(root: int, end: int, road: List[int]):
+            nonlocal graphMap, result
+            road.append(root)
+            if (root == end):
+                result.append(road)
+                return
+            elif (len(graphMap[root]) == 0):
+                return
+            else:
+                for child in graphMap[root]:
+                    test_road(child, end, road.copy())
+
+        test_road(0, len(graph)-1, [])
+        return result
