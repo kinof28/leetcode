e61b8baa50f13496c056c252fd54d92c72cf2220
Abdelwahab <ab28fb@gmail.com>
2022-12-31 00:19:24 +0100

    797. All Paths From Source to Target

--- 797. All Paths From Source to Target/Solution.java
+++ 797. All Paths From Source to Target/Solution.java
@@ -1,7 +1,32 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 class Solution {
+    List<List<Integer>> result = new LinkedList<>();
+
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        return null;
+        Map<Integer, int[]> graphMap = new HashMap<>();
+        for (int i = 0; i < graph.length; i++) {
+            graphMap.put(i, graph[i]);
+        }
+        testRoad(0, graph.length - 1, new LinkedList<>(), graphMap);
+        return result;
+    }
+
+    public void testRoad(int root, int end, List<Integer> road, Map<Integer, int[]> graphMap) {
+        road.add(root);
+        if (root == end) {
+            result.add(road);
+            return;
+        } else if (graphMap.get(root).length == 0)
+            return;
+        else {
+            for (int integer : graphMap.get(root)) {
+                testRoad(integer, end, new LinkedList<>(road), graphMap);
+            }
+        }
     }
 }
