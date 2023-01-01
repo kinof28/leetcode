b390fcd628afffa1e19ba232815bf20071006a6f
Abdelwahab <ab28fb@gmail.com>
2023-01-01 22:34:10 +0100

    587. Erect the Fence

--- 587. Erect the Fence/Solution.java
+++ 587. Erect the Fence/Solution.java
@@ -1,5 +1,40 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int[][] outerTrees(int[][] trees) {
-        return null;
+        if (trees.length < 4)
+            return trees;
+        int mostLeftTreeIndex = 0;
+        for (int i = 1; i < trees.length; i++) {
+            if (trees[mostLeftTreeIndex][0] < trees[i][0])
+                mostLeftTreeIndex = i;
+        }
+        Set<int[]> resultSet = new HashSet<>();
+        resultSet.add(trees[mostLeftTreeIndex]);
+        int current, candidate;
+        current = mostLeftTreeIndex;
+        while (true) {
+            candidate = (current + 1) % trees.length;
+            for (int i = 0; i < trees.length; i++) {
+                if (isMoreClockWise(trees[current], trees[candidate], trees[i]) > 0) {
+                    candidate = i;
+                }
+            }
+            for (int[] is : trees) {
+                if (isMoreClockWise(trees[current], trees[candidate], is) == 0)
+                    resultSet.add(is);
+            }
+            current = candidate;
+            if (current == mostLeftTreeIndex)
+                break;
+        }
+        return resultSet.toArray(new int[][] {});
+    }
+
+    private int isMoreClockWise(int[] firstPoint, int[] secondPoint, int[] thirdPoint) {
+
+        return ((thirdPoint[0] - firstPoint[0]) * (secondPoint[1] - firstPoint[1]))
+                - ((secondPoint[0] - firstPoint[0]) * (thirdPoint[1] - firstPoint[1]));
     }
 }
