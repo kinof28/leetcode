6e14b2a444b5e74ccf8bb724408604188673e308
Abdelwahab <ab28fb@gmail.com>
2023-01-01 01:41:39 +0100

    290. Word Pattern

--- 290. Word Pattern/Solution.java
+++ 290. Word Pattern/Solution.java
@@ -1,5 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (words.length != pattern.length())
+            return false;
+        Map<Character, String> map = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            if (map.containsKey(pattern.charAt(i))) {
+                if (!map.get(pattern.charAt(i)).equals(words[i]))
+                    return false;
+            } else {
+                if (map.containsValue(words[i]))
+                    return false;
+                map.put(pattern.charAt(i), words[i]);
+            }
+        }
         return true;
     }
 }
