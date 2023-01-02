d39602eb03f9309a61939254cc6e2486e8f1d1cd
Abdelwahab <ab28fb@gmail.com>
2023-01-02 22:59:46 +0100

    704. Binary Search

--- 704. Binary Search/Solution.py
+++ 704. Binary Search/Solution.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        return 0
+        min = 0
+        max = len(nums)-1
+        mid = -1
+        while (not (min == max and min == mid) and max >= min):
+            mid = int((max+min)/2)
+            if (nums[mid] == target):
+                return mid
+            elif (target > nums[mid]):
+                min = mid+1
+            elif (target < nums[mid]):
+                max = mid-1
+        return -1
