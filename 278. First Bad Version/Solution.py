2e499706682abb467dffcc2b1ec47cf8e5309aac
Abdelwahab <ab28fb@gmail.com>
2023-01-03 00:55:34 +0100

    278. First Bad Version

--- 278. First Bad Version/Solution.py
+++ 278. First Bad Version/Solution.py
@@ -3,4 +3,16 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        return 0
+        max: int = n
+        min: int = 0
+        mid = -1
+        while (not (min == max and min == mid) and max >= min):
+            mid = int((max+min)/2)+(max+min) % 2
+            if (isBadVersion(mid)):
+                if ((mid-1 == min) or not isBadVersion(mid-1)):
+                    return mid
+                else:
+                    max = mid-1
+            else:
+                min = mid
+        return n
