1cd0c3472ab6d5d320abf552d55775032196974c
Abdelwahab <ab28fb@gmail.com>
2023-01-02 03:24:28 +0100

    WIP

--- 587. Erect the Fence/Solution.py
+++ 587. Erect the Fence/Solution.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def outerTrees(self, trees: List[List[int]]) -> List[List[int]]:
-        return None
+        if (len(trees) < 4):
+            return trees
+        mostLeftTreeIndex: int = 0
+        for i in range(len(trees)):
+            if trees[i][0] < trees[mostLeftTreeIndex][0]:
+                mostLeftTreeIndex = i
+        currentTree = mostLeftTreeIndex
+        result = set()
+        while True:
+            candidate = (currentTree+1) % len(trees)
+
+        return result
