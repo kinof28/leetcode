74d654c213c4f55420c9214d380c63888d7bdcd8
Abdelwahab <ab28fb@gmail.com>
2023-01-03 01:27:55 +0100

    944. Delete Columns to Make Sorted

--- 944. Delete Columns to Make Sorted/Solution.py
+++ 944. Delete Columns to Make Sorted/Solution.py
@@ -3,4 +3,12 @@ from typing import List
 
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        return 0
+        sum: int = 0
+        for i in range(len(strs[0])):
+            previous_str: str = strs[0]
+            for string in strs[1:]:
+                if (previous_str[i] > string[i]):
+                    sum += 1
+                    break
+                previous_str = string
+        return sum
