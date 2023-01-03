b12a502da6b0525d3316aa7c7cd0fb7c612091e2
Abdelwahab <ab28fb@gmail.com>
2023-01-03 16:58:23 +0100

    35. Search Insert Position

--- 35. Search Insert Position/Solution.py
+++ 35. Search Insert Position/Solution.py
@@ -3,4 +3,18 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return 0
+        min: int = 0
+        max: int = len(nums)-1
+        while (min <= max):
+            mid: int = int((max+min)/2)+(max+min) % 2
+            if (nums[max] < target):
+                return max+1
+            if (nums[min] >= target):
+                return min
+            if (nums[mid] == target):
+                return mid
+            elif (nums[mid] > target):
+                max = mid-1
+            elif (nums[mid] < target):
+                min = mid+1
+        return mid
