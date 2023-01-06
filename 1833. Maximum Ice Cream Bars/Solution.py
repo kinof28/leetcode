44fbf2d044e568ac89fcab10b946265ce0b57f3e
Abdelwahab <ab28fb@gmail.com>
2023-01-06 11:04:28 +0100

    1833. Maximum Ice Cream Bars

--- 1833. Maximum Ice Cream Bars/Solution.py
+++ 1833. Maximum Ice Cream Bars/Solution.py
@@ -3,4 +3,12 @@ from typing import List
 
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        return 0
+        costs.sort()
+        sum = 0
+        for cost in costs:
+            if (cost > coins):
+                break
+            else:
+                coins -= cost
+                sum += 1
+        return sum
