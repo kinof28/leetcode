d3aba769b636f9ce49f16ddc6140778bbba63751
Abdelwahab <ab28fb@gmail.com>
2023-01-07 18:27:00 +0100

    brute force: time limite exceeded

--- 134. Gas Station/Solution.py
+++ 134. Gas Station/Solution.py
@@ -3,4 +3,24 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        return 0
+        # starting = 0
+        for i in range(len(gas)):
+            if (gas[i] >= cost[i]):
+                possible = True
+                available_gas = 0
+                starting = i
+                available_gas = gas[i]-cost[i]
+                j = (i+1) % len(gas)
+                while (possible):
+                    if (j == starting):
+                        return starting
+                    if ((gas[j]+available_gas) >= cost[j]):
+                        available_gas = (gas[j]+available_gas)-cost[j]
+                        j = (j+1) % len(gas)
+                    else:
+                        possible = False
+        return -1
+
+
+solution = Solution()
+print(solution.canCompleteCircuit([2, 3, 4], [3, 4, 3]))
