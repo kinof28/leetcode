92dcf3de20266ba6a5205121bced277a867c0911
Abdelwahab <ab28fb@gmail.com>
2023-01-10 02:53:54 +0100

    100. Same Tree

--- 100. Same Tree/Solution.py
+++ 100. Same Tree/Solution.py
@@ -11,4 +11,8 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return False
+        if (p == None and q == None):
+            return True
+        if(p==None or q == None):
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
