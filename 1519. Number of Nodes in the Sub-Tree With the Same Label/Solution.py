7917cd8dec310863c928f4d562f41fb6443aed67
Abdelwahab <ab28fb@gmail.com>
2023-01-12 23:13:51 +0100

    WIP

--- 1519. Number of Nodes in the Sub-Tree With the Same Label/Solution.py
+++ 1519. Number of Nodes in the Sub-Tree With the Same Label/Solution.py
@@ -1,6 +1,13 @@
 from typing import List
 
 
+class Node:
+    def __init__(self, val: str) -> None:
+        self.val = val
+        self.left = None
+        self.right = None
+
+
 class Solution:
     def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
         return None
