fdf10e2d52a7496d76be86ed9147a1f8f994202b
Abdelwahab <ab28fb@gmail.com>
2023-01-13 14:11:52 +0100

    19. Remove Nth Node From End of List

--- 19. Remove Nth Node From End of List/Solution.py
+++ 19. Remove Nth Node From End of List/Solution.py
@@ -10,4 +10,22 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        return None
+        def get_length() -> int:
+            nonlocal head
+            iterator = head
+            length = 0
+            while (iterator != None):
+                length += 1
+                iterator = iterator.next
+            return length
+        size = get_length()
+        if ((size-n+1) == 1):
+            head = head.next
+        else:
+            iterator = head
+            index = 1
+            while (index < size-n):
+                iterator = iterator.next
+                index += 1
+            iterator.next = iterator.next.next
+        return head
