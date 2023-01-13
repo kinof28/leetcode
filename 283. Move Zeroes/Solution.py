12612741b8cb9f9b7057a667a146000c807dfff7
Abdelwahab <ab28fb@gmail.com>
2023-01-13 13:07:14 +0100

    283. Move Zeroes

--- 283. Move Zeroes/Solution.py
+++ 283. Move Zeroes/Solution.py
@@ -6,3 +6,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        nums_copy = nums.copy()
+        i = 0
+        for num in nums_copy:
+            if (num != 0):
+                nums[i] = num
+                i += 1
+        for j in range(i, len(nums_copy)):
+            nums[j] = 0
