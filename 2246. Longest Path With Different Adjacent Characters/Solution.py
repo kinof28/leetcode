2b50f932fb5ad5f72b4feba2e668290227729a19
Abdelwahab <ab28fb@gmail.com>
2023-01-13 12:56:35 +0100

    WIP

--- 2246. Longest Path With Different Adjacent Characters/Solution.py
+++ 2246. Longest Path With Different Adjacent Characters/Solution.py
@@ -1,6 +1,28 @@
 from typing import List
 
 
+class Node:
+    def __init__(self, val: int) -> None:
+        self.val = val
+        self.children: List[Node] = []
+
+    def add_child(self, child) -> None:
+        self.children.append(child)
+
+
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        return 0
+
+        tree = dict()
+
+        def build_tree() -> None:
+            nonlocal tree
+            for i, p in enumerate(parent):
+                tree[i] = []
+
+                pass
+
+        def dfs() -> int:
+            return 0
+
+        return dfs()
