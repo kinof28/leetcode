c5aa7c1925d7e569eb264fd1f6e38659d497c8c5
Abdelwahab <ab28fb@gmail.com>
2023-01-13 14:41:31 +0100

    567. Permutation in String

--- 567. Permutation in String/Solution.py
+++ 567. Permutation in String/Solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if (len(s1) > len(s2)):
+            return False
+        s1 = sorted(s1)
+        for i in range(len(s2)-len(s1)+1):
+            sub = s2[i:i+len(s1)]
+            sub = sorted(sub)
+            if (sub == s1):
+                return True
         return False
