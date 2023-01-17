80779bb6a5bfede85c38475fb3f7b1c3b70bd26a
Abdelwahab <ab28fb@gmail.com>
2023-01-17 23:41:21 +0100

    926. Flip String to Monotone Increasing

--- 926. Flip String to Monotone Increasing/Solution.py
+++ 926. Flip String to Monotone Increasing/Solution.py
@@ -1,28 +1,10 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        leading_index = 0
-        ending_index = len(s)
-        for c in s:
-            if (c == '1'):
-                break
-            else:
-                leading_index += 1
-        for c in reversed(s):
-            if (c == '0'):
-                break
-            else:
-                ending_index -= 1
-        zeros = 0
-        ones = 0
-        for c in s[leading_index:ending_index]:
-            if (c == '0'):
-                zeros += 1
-            else:
+        ones, ans = 0, 0
+        for num in s:
+            if num == '1':
                 ones += 1
-        if (leading_index == 0):
-            return min(min(zeros, ones), 1+self.minFlipsMonoIncr(s[1:]))
-        return min(zeros, ones)
-
-
-solution = Solution()
-print(solution.minFlipsMonoIncr("10011111110010111011"))
+            elif ones:
+                ones -= 1
+                ans += 1
+        return ans
