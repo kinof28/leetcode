ab3fc2fc91bf1798f71a060b0e3127389eda3143
Abdelwahab <ab28fb@gmail.com>
2023-01-18 23:50:27 +0100

    918. Maximum Sum Circular Subarray

--- 918. Maximum Sum Circular Subarray/Solution.py
+++ 918. Maximum Sum Circular Subarray/Solution.py
@@ -3,4 +3,17 @@ from typing import List
 
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        return 0
+        if (len(nums) < 2):
+            return nums[0]
+        max_sum = nums[0]
+        min_sum = nums[0]
+        cur_min = 0
+        cur_max = 0
+        total_sum = 0
+        for num in nums:
+            cur_max = max(cur_max + num, num)
+            max_sum = max(max_sum, cur_max)
+            cur_min = min(cur_min + num, num)
+            min_sum = min(min_sum, cur_min)
+            total_sum += num
+        return max(max_sum, total_sum-min_sum)if max_sum > 0 else max_sum
