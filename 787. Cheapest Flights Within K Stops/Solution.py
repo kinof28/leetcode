634ba7448ddc570a3ec66b7611b44752a44c200b
Abdelwahab <ab28fb@gmail.com>
2023-02-01 15:50:56 +0100

    WIP

--- 787. Cheapest Flights Within K Stops/Solution.py
+++ 787. Cheapest Flights Within K Stops/Solution.py
@@ -3,4 +3,10 @@ from typing import List
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        if (k == 0):
+            for flight in flights:
+                if (flight[0] == src):
+                    if (flight[1] == dst):
+                        return flight[2]
+            return -1
         return 0
