ef2506e0f0fa186e4e543ab4bf08affdf1c3a172
Abdelwahab <ab28fb@gmail.com>
2023-02-16 00:19:14 +0100

    989. Add to Array-Form of Integer

--- 989. Add to Array-Form of Integer/Solution.py
+++ 989. Add to Array-Form of Integer/Solution.py
@@ -4,22 +4,15 @@ from typing import List
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
         extra = 0
-        print(num)
-        num = num[::-1]
         result = []
-        for n in reversed(num[:-1]):
+        for n in reversed(num):
             sum = n+int(k % 10)+extra
             extra = int(sum/10)
             result.append(int(sum % 10))
             k = int(k/10)
-        sum = num[-1]+k+extra
-        # result = result[::-1]
-        while (sum > 0):
+        while (extra > 0 or k > 0):
+            sum = int(k % 10)+extra
+            extra = int(sum/10)
             result.append(int(sum % 10))
-            sum = int(sum/10)
-        return result
-
-
-solution = Solution()
-print(solution.addToArrayForm(
-    [1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3], 516))
+            k = int(k/10)
+        return result[::-1]
