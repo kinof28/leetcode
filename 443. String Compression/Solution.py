03d725470d6ec9ed113c0e83a1e8389f57bfc5cc
Abdelwahab <ab28fb@gmail.com>
2023-03-02 04:57:32 +0100

    443. String Compression

--- 443. String Compression/Solution.py
+++ 443. String Compression/Solution.py
@@ -3,4 +3,21 @@ from typing import List
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        return 0
+        result = ""
+        i = 0
+        n = len(chars)
+        while i < n:
+            index = i+1
+            count = 1
+            while (index < n and chars[index] == chars[i]):
+                count = count+1
+                index = index+1
+            result = result+chars[i]
+            if (count > 1):
+                dist = str(count)
+                for c in dist:
+                    result = result+c
+            i = index
+        for i, c in enumerate(result):
+            chars[i] = c
+        return len(result)
