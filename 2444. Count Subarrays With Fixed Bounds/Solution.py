a50d6b1761c366f395908c57710a08a72e1f79c5
Abdelwahab <ab28fb@gmail.com>
2023-03-04 22:30:24 +0100

    WIP: Time Limit Exceeded

--- 2444. Count Subarrays With Fixed Bounds/Solution.py
+++ 2444. Count Subarrays With Fixed Bounds/Solution.py
@@ -3,4 +3,22 @@ from typing import List
 
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        return 0
+        sum = 0
+        for i in range(len(nums)):
+            min = nums[i]
+            max = nums[i]
+            if (min < minK or max > maxK):
+                continue
+            for n in nums[i:]:
+                if (n <= min):
+                    min = n
+                if (n >= max):
+                    max = n
+                if (min == minK and max == maxK):
+                    sum = sum+1
+        return sum
+
+
+solution = Solution()
+print(solution.countSubarrays([35054, 398719, 945315, 945315, 820417, 945315,
+      35054, 945315, 171832, 945315, 35054, 109750, 790964, 441974, 552913], 35054, 945315))
