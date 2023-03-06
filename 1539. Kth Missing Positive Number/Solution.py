87b5dd1052b9a9605d778e9e42d0f3b4971e0f0f
Abdelwahab <ab28fb@gmail.com>
2023-03-06 08:27:21 +0100

    1539. Kth Missing Positive Number

--- 1539. Kth Missing Positive Number/Solution.py
+++ 1539. Kth Missing Positive Number/Solution.py
@@ -3,4 +3,13 @@ from typing import List
 
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        return 0
+        missing = []
+        max_val = max(arr)
+        for i in range(1, max_val):
+            if (i not in arr):
+                missing.append(i)
+        while (len(missing) < k):
+            max_val = max_val+1
+            missing.append(max_val)
+
+        return missing[k-1]
