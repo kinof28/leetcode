0e32316ba8b4b7dc57fa99881399852a295945e9
Abdelwahab <ab28fb@gmail.com>
2023-03-07 06:08:06 +0100

    WIP: Time Limit Exceeded

--- 2187. Minimum Time to Complete Trips/Solution.py
+++ 2187. Minimum Time to Complete Trips/Solution.py
@@ -3,4 +3,20 @@ from typing import List
 
 class Solution:
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
-        return 0
+        if (len(time) == 1):
+            return totalTrips*time[0]
+        i = 0
+        time = sorted(time)
+        s = 0
+        while (s < totalTrips):
+            i = i+1
+            s = 0
+            for t in time:
+                if (t > i):
+                    break
+                s = s+int(i/t)
+        return i
+
+
+solution = Solution()
+print(solution.minimumTime([5, 10, 10], 9))
