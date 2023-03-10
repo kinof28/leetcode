980e9140dd422370481978ffc05a3f0af1c30108
Abdelwahab <ab28fb@gmail.com>
2023-03-10 13:36:50 +0100

    INIT

---
+++ 382. Linked List Random Node/Solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        pass
+
+    def getRandom(self) -> int:
+        return 0
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
