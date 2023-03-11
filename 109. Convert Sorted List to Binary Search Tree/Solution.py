35ce9d9c53e899a13ef24c1b782d5212e53874ba
Abdelwahab <ab28fb@gmail.com>
2023-03-11 04:10:51 +0100

    109. Convert Sorted List to Binary Search Tree

--- 109. Convert Sorted List to Binary Search Tree/Solution.py
+++ 109. Convert Sorted List to Binary Search Tree/Solution.py
@@ -18,4 +18,44 @@ class TreeNode:
 
 class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        return None
+        def get_list_length(head: Optional[ListNode]) -> int:
+            length = 0
+            while (head != None):
+                length = length+1
+                head = head.next
+            return length
+
+        def get_middle_node(head: Optional[ListNode], length: int) -> TreeNode:
+            if (head == None or length == 0):
+                return None
+            if (length == 1):
+                return TreeNode(head.val, None, None)
+            index = 0
+            p = head
+            pr = None
+            while (index < int(length/2) and p.next != None):
+                index = index+1
+                pr = p
+                p = p.next
+            if (pr != None):
+                pr.next = None
+            return TreeNode(p.val, get_middle_node(head, int(length/2)), get_middle_node(p.next, length-index-1))
+
+        return get_middle_node(head, get_list_length(head))
+
+
+solution = Solution()
+
+
+def print_tree(head: TreeNode) -> None:
+    if (head == None):
+        print("Null")
+
+        return
+    print(head.val)
+    print_tree(head.left)
+    print_tree(head.right)
+
+
+print_tree(solution.sortedListToBST(
+    ListNode(-1, ListNode(0, ListNode(1, ListNode(2,  None))))))
