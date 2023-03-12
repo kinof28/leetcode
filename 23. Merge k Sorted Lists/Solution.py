569c27b144481ceb450174c3dd191e5ad0003c4b
Abdelwahab <ab28fb@gmail.com>
2023-03-12 21:24:54 +0100

    WIP: Time Limit Exceeded

--- 23. Merge k Sorted Lists/Solution.py
+++ 23. Merge k Sorted Lists/Solution.py
@@ -10,4 +10,41 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        return None
+        def get_len() -> int:
+            nonlocal lists
+            length = 0
+            for l in lists:
+                if (l != None):
+                    length = length+1
+            return length
+
+        def get_min() -> Optional[ListNode]:
+            nonlocal lists
+            if (get_len() == 0):
+                return None
+            min_val = 10001
+            min_index = 0
+            for i, l in enumerate(lists):
+                if (l == None):
+                    continue
+                if (min_val > l.val):
+                    min_val = l.val
+                    min_index = i
+            lists[min_index] = lists[min_index].next
+            min_node = ListNode(min_val, get_min())
+            return min_node
+        return get_min()
+
+
+solution = Solution()
+node = solution.mergeKLists([ListNode(1, ListNode(4, ListNode(5))), ListNode(
+    1, ListNode(3, ListNode(4))), ListNode(2, ListNode(6))])
+
+
+def print_list(node: ListNode):
+    while (node != None):
+        print(f" -> {node.val}")
+        node = node.next
+
+
+print_list(node)
