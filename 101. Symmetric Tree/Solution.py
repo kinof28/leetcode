fcc1c4f2db1666e9da478db84096ed8df0c5e043
Abdelwahab <ab28fb@gmail.com>
2023-03-13 02:34:28 +0100

    101. Symmetric Tree

--- 101. Symmetric Tree/Solution.py
+++ 101. Symmetric Tree/Solution.py
@@ -1,5 +1,5 @@
 # Definition for a binary tree node.
-from typing import Optional
+from typing import List, Optional
 
 
 class TreeNode:
@@ -11,4 +11,25 @@ class TreeNode:
 
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return False
+        def is_array_symmetric(array) -> bool:
+            n = len(array)
+            for i in range(int(n/2)):
+                if (array[i] == None and array[n-1-i] == None):
+                    continue
+                if (array[i] == None or array[n-1-i] == None):
+                    return False
+                if (array[i].val != array[n-1-i].val):
+                    return False
+            return True
+
+        def bfs(nodes: List[TreeNode]) -> bool:
+            if (len(nodes) == 0):
+                return True
+            nodes_to_visit = []
+            for node in nodes:
+                if (node == None):
+                    continue
+                nodes_to_visit.append(node.left)
+                nodes_to_visit.append(node.right)
+            return is_array_symmetric(nodes) and bfs(nodes_to_visit)
+        return bfs([root])
